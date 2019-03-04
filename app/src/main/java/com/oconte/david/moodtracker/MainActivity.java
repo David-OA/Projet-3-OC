bf142c5d7c122d6f296cca9814b4ee3ba81c9ae9
David.OA <david.oconte@orange.fr>
2019-03-04 17:06:28 +0100

    I add the AlertDialog with the EditText.

--- app/src/main/java/com/oconte/david/moodtracker/MainActivity.java
+++ app/src/main/java/com/oconte/david/moodtracker/MainActivity.java
@@ -1,10 +1,14 @@
 package com.oconte.david.moodtracker;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Gallery;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -22,6 +26,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /////////////////////////////////////////////////////////////////////
+        // Partie sur le boutton smiley a modifier pour le swip plutard/////
+        ///////////////////////////////////////////////////////////////////
         smiley_botton = (ImageButton) findViewById(R.id.smiley_botton);
         smiley_botton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,15 +37,49 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        /////////////////////////////////////////////////////////////////////
+        // Partie sur le boutton commentaire                           /////
+        ///////////////////////////////////////////////////////////////////
         note_button = (ImageButton) findViewById(R.id.note_button);
         note_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "youpi dou " , Toast.LENGTH_LONG ).show();
+                //Toast.makeText(MainActivity.this, "youpi dou " , Toast.LENGTH_LONG ).show();
+
+                LayoutInflater factory = LayoutInflater.from(MainActivity.this);
+                final View alertDialogView = factory.inflate(R.layout.dialog_comment, null);
+
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
+                alertDialog.setMessage("Commentaire");
+
+                alertDialog.setView(alertDialogView);
+
+                alertDialog.setPositiveButton("VALIDER", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int arg1) {
+
+                    }
+                });
+                alertDialog.setNegativeButton("ANNULER", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int arg1) {
+
+                    }
+                });
+                alertDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                    @Override
+                    public void onCancel(DialogInterface dialog) {
+
+                    }
+                });
+                alertDialog.show();
             }
         });
 
-        //Bouton de de l'activity history
+
+        /////////////////////////////////////////////////////////////////////
+        // Partie sur le boutton history                               /////
+        ///////////////////////////////////////////////////////////////////
         history_botton = (ImageButton) findViewById(R.id.history_botton);
         history_botton.setOnClickListener(new View.OnClickListener() {
             @Override
