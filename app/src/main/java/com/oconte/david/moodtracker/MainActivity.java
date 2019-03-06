17c1ccdec51564a145774a2f1fdc1fd06cc922bf
David.OA <david.oconte@orange.fr>
2019-03-06 17:02:28 +0100

    I add my swipe methode.

--- app/src/main/java/com/oconte/david/moodtracker/MainActivity.java
+++ app/src/main/java/com/oconte/david/moodtracker/MainActivity.java
@@ -1,5 +1,6 @@
 package com.oconte.david.moodtracker;
 
+import android.annotation.SuppressLint;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -15,34 +16,56 @@ import android.widget.Gallery;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageButton smiley_botton;
     ImageButton note_button;
     ImageButton history_botton;
+
+    ImageView smiley_swipe;
 
     private static final String COMMENT = "COMMENT";
     SharedPreferences sharedPreferences;
 
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /////////////////////////////////////////////////////////////////////
-        // Partie sur le boutton smiley a modifier pour le swip plutard/////
-        ///////////////////////////////////////////////////////////////////
-        smiley_botton = (ImageButton) findViewById(R.id.smiley_botton);
-        smiley_botton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        //////////////////////////////////////////////////////////////////////
+        ///        Partie sur le swipe                                    ///
+        ////////////////////////////////////////////////////////////////////
 
-                smiley_botton.setImageResource(R.drawable.smiley_sad);
-                smiley_botton.setBackgroundResource(R.color.faded_red);
+        initializeView();
+        smiley_swipe.setOnTouchListener(new OnSwipeTouchListener(MainActivity.this) {
+            public void onSwipeTop() {
+                Toast.makeText(getApplicationContext(), getResources().getString(R.string.toastTop), Toast.LENGTH_SHORT).show();
+                //smiley_swipe.setImageResource(R.drawable.smiley_normal);
+                //smiley_swipe.setBackgroundResource(R.color.cornflower_blue_65);
+
+                //smiley_swipe.setImageResource(R.drawable.smiley_disappointed);
+                //smiley_swipe.setBackgroundResource(R.color.warm_grey);
+
+                //smiley_swipe.setImageResource(R.drawable.smiley_sad);
+                //smiley_swipe.setBackgroundResource(R.color.faded_red);
+            }
+            public void onSwipeRight() {
+                Toast.makeText(getApplicationContext(), getResources().getString(R.string.toastRight), Toast.LENGTH_SHORT).show();
+            }
+            public void onSwipeLeft() {
+                Toast.makeText(getApplicationContext(), getResources().getString(R.string.toastLeft), Toast.LENGTH_SHORT).show();
+            }
+            public void onSwipeBottom() {
+                Toast.makeText(getApplicationContext(), getResources().getString(R.string.toastBottom), Toast.LENGTH_SHORT).show();
+                //smiley_swipe.setImageResource(R.drawable.smiley_super_happy);
+                //smiley_swipe.setBackgroundResource(R.color.banana_yellow);
             }
         });
+
 
         /////////////////////////////////////////////////////////////////////
         // Partie sur le boutton commentaire                           /////
@@ -64,7 +87,7 @@ public class MainActivity extends AppCompatActivity {
                 alertDialog.setPositiveButton("VALIDER", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int arg1) {
-                        
+
                         sharedPreferences = getBaseContext().getSharedPreferences(COMMENT, MODE_PRIVATE);
                         //sauvegarder le commentaire
                         sharedPreferences
@@ -72,6 +95,7 @@ public class MainActivity extends AppCompatActivity {
                                 .putString(COMMENT, "")
                                 .apply();
 
+                        // String COMMENT = preferences.getString(COMMENT, "")
                     }
                 });
                 alertDialog.setNegativeButton("ANNULER", new DialogInterface.OnClickListener() {
@@ -103,5 +127,9 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void initializeView() {
+        smiley_swipe = (ImageView) findViewById(R.id.smiley_swipe);
     }
 }
